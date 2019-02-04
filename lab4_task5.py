d54f27c8ce7ac5860a1c6bd1c278385e3a618928
yangning172 <yn8065@gmail.com>
2019-02-04 00:25:56 -0400

    task5

---
+++ lab4_task5.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as pyplot
+pyplot.clf()
+pyplot.xscale(scale)
+pylot.yscalse(scale)
+
+
+"""Dear Professor:
+   I tried many times to make clear this way, but I can not understand completely"""
